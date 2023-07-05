c4a4cf8e3e128b9ac81f3af87d5952856419c0bc
Daniel Micay <daniel.micay@grapheneos.org>
2023-07-05 08:58:05 -0400

    Revert "BACKPORT: f2fs: give priority to select unpinned section for foreground GC"
    This reverts commit c8ff430b7f793034ee9193c8415e61fa2f38cc7b.

--- fs/f2fs/segment.c
+++ fs/f2fs/segment.c
@@ -4767,13 +4767,6 @@ static int init_victim_secmap(struct f2fs_sb_info *sbi)
 	dirty_i->victim_secmap = f2fs_kvzalloc(sbi, bitmap_size, GFP_KERNEL);
 	if (!dirty_i->victim_secmap)
 		return -ENOMEM;
-
-	dirty_i->pinned_secmap = f2fs_kvzalloc(sbi, bitmap_size, GFP_KERNEL);
-	if (!dirty_i->pinned_secmap)
-		return -ENOMEM;
-
-	dirty_i->pinned_secmap_cnt = 0;
-	dirty_i->enable_pin_section = true;
 	return 0;
 }
 
@@ -5298,7 +5291,6 @@ static void destroy_victim_secmap(struct f2fs_sb_info *sbi)
 {
 	struct dirty_seglist_info *dirty_i = DIRTY_I(sbi);
 
-	kvfree(dirty_i->pinned_secmap);
 	kvfree(dirty_i->victim_secmap);
 }
 
